c2baf376a39b2dbea73a46554c80a0c1ef9d04b9
Justin Pattarukuzhyil Jose <justin.p.jose@ons.gov.uk>
2022-09-13 08:52:42 +0100

    remove locking in mock healthCheckStatusHandle

--- features/steps/helper.go
+++ features/steps/helper.go
@@ -8,8 +8,6 @@ import (
 
 func healthCheckStatusHandle(status int) httpfake.Responder {
 	return func(w http.ResponseWriter, r *http.Request, rh *httpfake.Request) {
-		rh.Lock()
-		defer rh.Unlock()
 		w.WriteHeader(status)
 	}
 }
